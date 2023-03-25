4099214542c769a69d6ee10b00d65749b3bbbb36
bioneisme <bioneisme@gmail.com>
2023-03-25 13:45:00 +0600

    Fixed getApplications

--- backend/routes/applicationRoute.ts
+++ backend/routes/applicationRoute.ts
@@ -5,7 +5,7 @@ import authMiddleware from "../middlewares/authMiddleware";
 
 const router: Router = Router();
 
-router.get("/getApplications", applicationController.getMyApplications); // for admin
+router.get("/getApplications", applicationController.getApplications); // for admin
 router.get("/getApplicationById/:id", applicationController.getApplicationById); // for admin
 router.get("/getApplicationsByUser/:id", applicationController.getApplicationsByUser); // for admin
 router.get("/getApplicationsByStatus/:status", applicationController.getApplicationsByStatus); // for admin
